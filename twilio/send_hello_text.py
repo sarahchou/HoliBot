fe4a368e1aef018c92fcb3cd79f90a809c066357
sarahchou <chou.s@husky.neu.edu>
2018-03-18 02:00:25 -0700

    changed secret keys

--- twilio/send_hello_text.py
+++ twilio/send_hello_text.py
@@ -3,13 +3,13 @@
 from twilio.rest import Client
 
 # Find these values at https://twilio.com/user/account
-account_sid = "AC0cd74b039689afeb86f7f9909fae8ea5"
-auth_token = "6a1cc56819a834b67afa3f43023257a4"
+account_sid = "your-sid-here"
+auth_token = "your-token-here"
 
 client = Client(account_sid, auth_token)
 
 client.api.account.messages.create(
-    to="15105007810", #your phone number here
+    to="14152224567", #your phone number here
     from_="+14152125236", #my twilio number
     body="Hello there! I am HoliBot. Reply with a date in March in the format "
          "(month day) for a random weird holiday that falls on that day. For example: March 15")
