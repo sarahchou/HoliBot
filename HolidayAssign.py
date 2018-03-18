f98b3ac6a6d9498206fedd6496c392de32c3290f
sarahchou <chou.s@husky.neu.edu>
2018-03-17 23:24:43 -0700

    Fixed the dictionary creation

--- HolidayAssign.py
+++ HolidayAssign.py
@@ -1,4 +1,4 @@
-from datetime import date
+import collections
 
 
 class HolidayAssign:
@@ -6,42 +6,18 @@ class HolidayAssign:
         self._create_dict = {}
 
     @staticmethod
-    def file_len(f_name):
-        with open(f_name) as f:
-            for i, l in enumerate(f):
-                pass
-        return i + 1
-
-    @staticmethod
     def create_dict():
-        count = 0
         # open the files
         march_holidays = open("marchholidays.txt", "r")
-        march_dates = open("marchdates.txt", "r")
 
-        current_date = march_dates.readline(count)
-        current_holiday = march_holidays.readline(count)
-
-        num_dates = HolidayAssign.file_len("marchdates.txt")
         # initialize an empty dictionary
-        holidays = {}
+        holidays = collections.OrderedDict()
 
         # loop through the dates in the dates file
-        while count < num_dates:
-            holidays[march_dates.readline(count)] = march_holidays.readline(count)
-            # at each date key, place
-            # holidays[count] = march_dates.readline(count)
-            count += 1
+        with open('marchdates.txt') as f:
+            for line in f:
+                holidays[line.rstrip()] = march_holidays.readline()
 
-        # count2 = 0
-        # for holiday in march_holidays:
-        #     holidays[holidays[count2]] = march_holidays.readline(count2)
-        #     count2 += 1
-
-        # Debugging by printing dictionary contents
-        for d in holidays:
-            print(d)
-            print(holidays[d])
         return holidays
 
     def output(self, body):
