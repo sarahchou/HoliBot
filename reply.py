6af4e781da7ffc6c4b57409ff73efc9867dc7d66
sarahchou <chou.s@husky.neu.edu>
2018-03-18 01:38:14 -0700

    i forgot

--- reply.py
+++ reply.py
@@ -4,6 +4,19 @@ from HolidayAssign import HolidayAssign
 
 app = Flask(__name__)
 
+def format_stars(holiday):
+    s = "*=*=*=*=*=*=*="
+    i = 0
+
+    while i < len(holiday):
+        if i % 2 == 0:
+            s += "*"
+        else:
+            s += "="
+        i += 1
+
+    return s
+
 
 @app.route("/sms", methods=['GET', 'POST'])
 def incoming_sms():
@@ -18,7 +31,13 @@ def incoming_sms():
     holidayList = HolidayAssign()
     date_holiday_dict = holidayList.create_dict()
 
-    if date in date_holiday_dict.keys():
+    if date == "Hi":
+        resp.message(
+            "Hello there! I am HoliBot, created by Sarah and Vaughn. Text me a date in March in the format "
+            "(month day) for a random weird holiday that falls on that day. For example: March 05")
+
+    elif date in date_holiday_dict.keys():
+        #stars = format_stars(date_holiday_dict[date])
         resp.message(date + " is " + date_holiday_dict[date])
 
     # Determine the right reply for this message
@@ -28,7 +47,7 @@ def incoming_sms():
         resp.message("Goodbye")
     elif not date in date_holiday_dict.keys():
         resp.message(
-            "The message you typed is currently unsupported. Please reply with either a valid March date, "
+            "The message you typed is currently unsupported. Perhaps the date has no weird holiday yet. Please reply with either a valid March date, "
             "or the words hello or bye")
 
     return str(resp)
