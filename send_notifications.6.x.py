7cb04965143d28c718d7100c6926a264878f7f4a
sarahchou <chou.s@husky.neu.edu>
2018-03-17 17:28:34 -0700

    updated

--- send_notifications.6.x.py
+++ send_notifications.6.x.py
@@ -3,12 +3,12 @@
 from twilio.rest import Client
 
 # Find these values at https://twilio.com/user/account
-account_sid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-auth_token = "your_auth_token"
+account_sid = "AC0cd74b039689afeb86f7f9909fae8ea5"
+auth_token = "6a1cc56819a834b67afa3f43023257a4"
 
 client = Client(account_sid, auth_token)
 
 client.api.account.messages.create(
     to="+14156806162",
-    from_="+15555555555",
+    from_="+4152125236",
     body="Hello there!")
